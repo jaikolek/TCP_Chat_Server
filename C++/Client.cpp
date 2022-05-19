9c40a2b5b3bea4a3d2610eea0e2bfc8bc3841f70
Daniel Danang <84694349+jaikolek@users.noreply.github.com>
2022-05-19 09:17:34 +0700

    Add files via upload

---
+++ C++/Client.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#include <thread>
+#pragma comment (lib, "ws2_32.lib")
+
+void recvSocket(SOCKET sock)
+{
+	char buf[4096];
+	while (1)
+	{
+		ZeroMemory(buf, 4096);
+		int bytesReceived = recv(sock, buf, 4096, 0);
+		if (bytesReceived > 0)
+		{
+			std::cout << "FROM SERVER> " << std::string(buf, 0, bytesReceived) << std::endl;
+		}
+	}
+}
+
+int main()
+{
+	std::string ipAddress = "127.0.0.1";	// IP Address of the server
+	int port = 12000;				// Listening port number on the server
+
+	// Initialize Winsock
+	WSAData data; // WinSock data-structure
+	WORD ver = MAKEWORD(2, 2); // 2.2 version
+
+	int wsResult = WSAStartup(ver, &data); //init winsock lib
+	if (wsResult != 0)
+	{
+		std::cout << "Winsock can't restart, Error in #" << wsResult << std::endl;
+		return 1;
+	}
+
+	// Create socket
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		std::cout << "Socket can't create, Error in #" << WSAGetLastError() << std::endl;
+		WSACleanup();
+		return 1;
+	}
+
+	// Fill in a hint structure
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+	// Connect to server
+	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR)
+	{
+		std::cout << "Can`t connect to server, Error in #" << WSAGetLastError() << std::endl;
+		closesocket(sock);
+		WSACleanup();
+		return 1;
+	}
+
+	// thread to receive data
+	std::thread recv_thread(recvSocket, sock);
+
+	// Do-while loop to send data
+	std::string userInput;
+	do
+	{
+		// Prompt the user for some text
+		std::getline(std::cin, userInput);
+		if (userInput.size() > 0) // Make sure the user has typed in something
+		{
+			// Send the text
+			// std::cout << "You: ";
+			send(sock, userInput.c_str(), userInput.size() + 1, 0);
+			std::cout << std::endl;
+		}
+	} while (userInput.size() > 0);
+
+	// close
+	recv_thread.join();
+	closesocket(sock);
+	WSACleanup();
+
+	// pause system for exe file
+	system("pause");
+	return 0;
+}
---
+++ C++/Server.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#pragma comment(lib,"ws2_32.lib")
+
+std::vector <std::string> tmpChats;
+
+void saveChat(std::vector <std::string> v)
+{
+	std::ofstream file;
+	file.open("chat_log.txt");
+	for (int i = 0; i < v.size(); ++i)
+	{
+		file << v[i] << std::endl;
+	}
+	file.close();
+}
+
+int main()
+{
+	int port = 12000;
+
+	// Initialize Winsock
+	WSADATA wsData; // WinSock data-structure
+	WORD ver = MAKEWORD(2, 2); // means 2.2 version
+
+	int wsOk = WSAStartup(ver, &wsData); // initializing Winsock lib 
+	std::cout << "WinSock Started!!" << std::endl;
+
+	if (wsOk != 0)
+	{
+		std::cout << "Can`t Initialize Winsock! Quitting" << std::endl;
+		return 1;
+	}
+
+	// Create a socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0); // make socket with IPv4, TCP (SOCK_DGRAM:UDP)
+	std::cout << "Socket Created" << std::endl;
+
+	if (listening == INVALID_SOCKET)
+	{
+		std::cout << "Can`t create a socket! Quitting" << std::endl;
+		return 1;
+	}
+
+	// Bind the socket to an ip address and port to a socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port); // host byte to network byte (little->big endian)
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+	// Tell Winsock the socket is for listening
+	listen(listening, SOMAXCONN);
+
+	fd_set master; // data-structrue that contains list of socket, and their stats
+	FD_ZERO(&master);
+	
+	FD_SET(listening, &master);
+
+	bool running = true;
+
+	while (running)
+	{
+		// select function cause chaning of fd. so we need copy of fd
+		fd_set copy = master;
+		int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
+
+		for (int i = 0; i < socketCount; i++)
+		{
+
+			SOCKET sock = copy.fd_array[i];
+
+			if (sock == listening)
+			{
+				// Accept a new connection
+				SOCKET client = accept(listening, nullptr, nullptr);
+
+				// Add the new connection to the list of connected clients
+				FD_SET(client, &master);
+
+				// Send a welcome message to the connected client
+				std::string welcomeMsg = "Welcome to my Chat Server!\r\n";
+				
+				send(client, welcomeMsg.c_str(), welcomeMsg.size() + 1, 0);
+
+				// broadcast welcome message
+				std::ostringstream ss;
+				ss << "Client" << " #" << client << " connected.\r\n";
+				std::string strOut = ss.str();
+				for (int i = 0; i < master.fd_count; i++)
+				{
+					SOCKET outSock = master.fd_array[i];
+					if (outSock != listening && outSock != client)
+					{
+						send(outSock, strOut.c_str(), strOut.size() + 1, 0);
+					}
+				}
+				// logging welcome message to server
+				std::cout << strOut << std::endl;
+			}
+			else
+			{
+				char buf[4096];
+				ZeroMemory(buf, 4096);
+
+				// Receive Message
+				int bytesIn = recv(sock, buf, 4096, 0);
+				if (bytesIn <= 0)
+				{
+					// Drop the client
+					closesocket(sock);
+					FD_CLR(sock, &master);
+				}
+				else
+				{
+					// Check to see if it`s a command.  \quit kills the server
+					if (buf[0] == '\\')
+					{
+						std::string cmd = std::string(buf, bytesIn);
+						if (cmd == "\\quit")
+						{
+							running = false;
+							break;
+						}
+					}
+
+					// Send message to other client, and definiately not the listening socket
+					std::ostringstream ss;
+					ss << "Client #" << sock << ":" << buf << "\r\n";
+					std::string strOut = ss.str();
+					for (int i = 0; i < master.fd_count; i++)
+					{
+						SOCKET outSock = master.fd_array[i];
+						if (outSock != listening && sock != outSock)
+						{
+							send(outSock, strOut.c_str(), strOut.size() + 1, 0);
+						}
+					}
+					// logging user`s message to server
+					std::cout << strOut << std::endl;
+
+					tmpChats.push_back(strOut);
+					saveChat(tmpChats);
+				}
+			}
+		}
+	}
+
+	// Close listening socket
+	FD_CLR(listening, &master);
+	closesocket(listening);
+
+	// Cleanup Winsock
+	WSACleanup();
+
+	// pause system for exe file
+	system("pause");
+	return 0;
+}
